0869e860a6a8cb4ddbde6d0a917d819d56121bf1
summerwaves <z7251007@163.com>
2018-02-05 20:37:26 +0800

    fuck you

--- chapter21/src/main/java/cn/summerwaves/Executor/CachedTreadPool.java
+++ chapter21/src/main/java/cn/summerwaves/Executor/CachedTreadPool.java
@@ -7,13 +7,10 @@ import java.util.concurrent.Executors;
 
 public class CachedTreadPool {
     public static void main(String[] args) {
-        Long start = System.currentTimeMillis();
         ExecutorService exec = Executors.newCachedThreadPool();
         for (int i = 0;i<5;i++) {
             exec.execute(new LiftOff());
         }
         exec.shutdown();
-        Long end = System.currentTimeMillis();
-        System.out.println("CachedTreadPool运行共用了 " + (end - start) + " ms");
     }
 }
--- chapter21/src/main/java/cn/summerwaves/Executor/FixThreadPool.java
+++ chapter21/src/main/java/cn/summerwaves/Executor/FixThreadPool.java
@@ -7,13 +7,10 @@ import java.util.concurrent.Executors;
 
 public class FixThreadPool {
     public static void main(String[] args) {
-        Long start = System.currentTimeMillis();
         ExecutorService exec = Executors.newFixedThreadPool(5);
         for (int i=0;i<5;i++) {
             exec.execute(new LiftOff());
         }
         exec.shutdown();
-        Long end = System.currentTimeMillis();
-        System.out.println("FixThreadPool运行共用了 " + (end - start) + " ms");
     }
 }
